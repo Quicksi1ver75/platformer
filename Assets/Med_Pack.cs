42d78eca4b4d3d8fe9eecb697f50eb9b89d43024
Quicksi1ver75 <h.tulinska75@gmail.com>
2026-03-25 10:58:38 +0100

    miau
    s

---
+++ Assets/Med_Pack.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Med_Pack : MonoBehaviour
+{
+    public float heal = 1;
+
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.GetComponent<health_component>().AddHealth(heal);
+        Destroy(gameObject);
+    }
+   
+}
